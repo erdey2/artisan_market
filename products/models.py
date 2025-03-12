4d43984ba32afed4d595e1904ef08a9cbb52be6f
Erdey <erdeysyoum@gmail.com>
2025-03-12 15:08:51 +0300

    product model added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    artisan = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products', limit_choices_to={'role': 'artisan'})
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
