eb2725b95a59acd30bc9c32f2f85fb5d0ebae1ff
Erdey <erdeysyoum@gmail.com>
2025-03-13 11:25:54 +0300

    apply permissions to product views

--- products/views.py
+++ products/views.py
@@ -1,13 +1,18 @@
 from rest_framework import generics
 from .serializers import ProductSerializer
 from products.models import Product
-
+from .permissions import IsArtistOrReadOnly # import the new custom permissions class
 
 class ProductListView(generics.ListCreateAPIView):
+    """ Artist can create products; everyone can browse."""
     serializer_class = ProductSerializer
+    permission_classes = [IsArtistOrReadOnly] # apply permissions
 
     def get_queryset(self):
+        """ Customers can browse all products; Artist see only their own."""
         qs = Product.objects.all()
+
+        # apply filtering
         name = self.request.GET.get('name')
         price = self.request.GET.get('price')
         if name:
@@ -16,6 +21,10 @@ class ProductListView(generics.ListCreateAPIView):
             qs = qs.filter(price__lte=price)
         return qs
 
+    def perform_create(self, serializer):
+        """Assign the currently logged-in artisan as the product's artist."""
+        serializer.save(artist=self.request.user)
+    
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
@@ -23,9 +32,10 @@ class ProductListView(generics.ListCreateAPIView):
         return self.create(request, *args, **kwargs)
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    """Retrieve, update, or delete a product."""
+    """Retrieve, update, or delete a product (only by owner)."""
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsArtistOrReadOnly]
 
 
     def get(self, request, *args, **kwargs):
