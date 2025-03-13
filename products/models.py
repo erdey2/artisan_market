a148c3db864cf8eba9dd6a6e2c59033e5ed3c816
Erdey <erdeysyoum@gmail.com>
2025-03-13 10:58:17 +0300

    indexing applying to name and price for efficient data retrival

--- products/models.py
+++ products/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index = True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index = True)
     stock = models.PositiveIntegerField()
     artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)
